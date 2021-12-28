795ea4bdfc743e9df65f2a859d661ec82979a593
Gabriel Thibeault <gabriel.eric.thibeault@gmail.com>
2021-12-28 19:15:36 +0100

    Empty method and UTs and custom key extract min UT

--- src/data_structures/binary_heap.py
+++ src/data_structures/binary_heap.py
@@ -1,59 +1,61 @@
-from functools import reduce
 class BinaryHeap(object):
-    def __init__(self, key = None) -> None:
+    def __init__(self, key=None) -> None:
         self.values = []
-        
+
         if key is not None:
             self.key = key
         else:
             self.key = self.__default_key
-        
+
+    def is_empty(self):
+        return len(self.values) == 0
+
     def peek_min(self):
         if self.values:
             return self.values[0]
         else:
             return None
-        
+
     def insert(self, val) -> None:
         self.values.append(val)
         current_index = len(self.values) - 1
-        
+
         self.__heap_up(current_index)
-        
+
     def extract_min(self):
         if not self.values:
             raise LookupError("Empty heap")
 
         self.values[0], self.values[-1] = self.values[-1], self.values[0]
         min = self.values.pop()
-        
+
         self.__heap_down(0)
-        
+
         return min
-        
+
     def __heap_down(self, i) -> None:
         while i < len(self.values):
             child_indices = self.__get_children_indices(i)
-            
+
             m = i
             for c in child_indices:
                 if c < len(self.values):
                     if self.key(self.values[c], self.values[m]):
                         m = c
-            
+
             if m == i:
                 break
             else:
-                self.values[m], self.values[i] = self.values[i], self.values[m] 
+                self.values[m], self.values[i] = self.values[i], self.values[m]
                 i = m
-            
-    def __heap_up(self, i) -> None:        
+
+    def __heap_up(self, i) -> None:
         while i != 0:
             parent_index = self.__get_parent_index(i)
-            
+
             if self.key(self.values[i], self.values[parent_index]):
                 self.values[i], self.values[parent_index] = self.values[parent_index], self.values[i]
-                
+
                 i = parent_index
             else:
                 break
@@ -62,7 +64,7 @@ class BinaryHeap(object):
         base = 2 * i
 
         return base + 1, base + 2
-    
+
     def __get_parent_index(self, i):
         return (i - 1) // 2
 
--- test/data_structures/binary_heap_test.py
+++ test/data_structures/binary_heap_test.py
@@ -2,13 +2,29 @@ from src.data_structures.binary_heap import BinaryHeap
 
 import unittest
 
+
 class TestBinaryHeap(unittest.TestCase):
+    def test_empty_true(self):
+        heap = BinaryHeap()
+
+        empty = heap.is_empty()
+
+        self.assertTrue(empty)
+
+    def test_empty_false(self):
+        heap = BinaryHeap()
+
+        heap.insert(1)
+        empty = heap.is_empty()
+
+        self.assertFalse(empty)
+
     def test_insert_respects_min(self):
         heap = BinaryHeap()
-        
+
         n = 45
         data = list(range(n, -1, -1))
-        
+
         for d in data:
             heap.insert(d)
 
@@ -17,68 +33,83 @@ class TestBinaryHeap(unittest.TestCase):
         self.assertEqual(min, 0)
 
     def test_insert_respects_min_with_custom_key(self):
-        gt_func = lambda l, r: l > r
+        def gt_func(l, r): return l > r
         heap = BinaryHeap(key=gt_func)
-        
+
         n = 45
         data = list(range(n, -1, -1))
-        
+
         for d in data:
             heap.insert(d)
 
         max = heap.peek_min()
 
         self.assertEqual(max, n)
-        
+
     def test_empty_peek_min_returns_none(self):
         heap = BinaryHeap()
 
         min = heap.peek_min()
 
         self.assertIsNone(min)
-        
+
     def test_extract_min_on_empty_raises(self):
         heap = BinaryHeap()
 
         with self.assertRaises(LookupError):
             heap.extract_min()
-        
+
     def test_trivial_extract_min(self):
         heap = BinaryHeap()
 
         v = 1
         heap.insert(v)
-        
+
         m = heap.extract_min()
 
         self.assertEqual(m, v)
-        
+
     def test_extract_min_removes(self):
         heap = BinaryHeap()
 
         v = 1
         heap.insert(v)
         heap.extract_min()
-        
+
         m = heap.peek_min()
 
         self.assertIsNone(m)
-        
+
     def test_extract_min_extracts_minimum(self):
         heap = BinaryHeap()
-        
+
         n = 45
         data = list(range(n, -1, -1))
-        
+
         for d in data:
             heap.insert(d)
 
         res = []
-        while heap.peek_min() is not None:
+        while not heap.is_empty():
             m = heap.extract_min()
             res.append(m)
 
         expected = list(range(0, n + 1))
-        self.assertEqual(len(expected), len(res))
-        for d, r in zip(expected, res):
-            self.assertEqual(d, r)
+        self.assertEqual(expected, res)
+
+    def test_extract_min_extracts_minimum_with_custom_key(self):
+        def gt_func(l, r): return l > r
+        heap = BinaryHeap(key=gt_func)
+
+        n = 45
+        data = list(range(n, -1, -1))
+
+        for d in data:
+            heap.insert(d)
+
+        res = []
+        while not heap.is_empty():
+            m = heap.extract_min()
+            res.append(m)
+
+        self.assertEqual(data, res)
