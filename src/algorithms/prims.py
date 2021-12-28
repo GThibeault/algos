68c5f838689178a868018797a4d582b082be28ef
Gabriel Thibeault <gabriel.eric.thibeault@gmail.com>
2021-12-28 22:17:39 +0100

    Prim's fixes and some UTs

--- src/algorithms/prims.py
+++ src/algorithms/prims.py
@@ -3,40 +3,53 @@ from src.data_structures.tracking_binary_heap import TrackingBinaryHeap
 
 class Prims(object):
     def get_mst(self, adjacency_matrix):
-        heap = TrackingBinaryHeap()
+        def key_func(l, r): return l.weight <= r.weight
+        heap = TrackingBinaryHeap(key=key_func)
 
-        picked = [False for i in range(adjacency_matrix)]
-        costs = [None for i in range(adjacency_matrix)]
-        edges = [None for i in range(adjacency_matrix)]
+        picked = [False for i in range(len(adjacency_matrix))]
+        costs = [None for i in range(len(adjacency_matrix))]
+        edges = [None for i in range(len(adjacency_matrix))]
 
         result = []
         sum = 0
 
         start_vertex = 0
-        heap.insert(start_vertex)
+        start_entry = Entry(0, 0)
+        heap.insert(start_entry)
 
         while not heap.is_empty():
-            current_vertex = heap.extract_min()
+            current_vertex, weight = heap.extract_min()
             picked[current_vertex] = True
 
             if current_vertex != start_vertex:
                 neighbor = edges[current_vertex]
 
-                result.append(current_vertex, neighbor)
-                sum += adjacency_matrix[current_vertex][neighbor][1]
+                result.append((current_vertex, neighbor))
+                sum += weight
 
-            for vertex, edge_weight in adjacency_matrix[current_vertex]:
-                if picked[vertex]:
+            for vertex, edge_weight in enumerate(adjacency_matrix[current_vertex]):
+                if picked[vertex] or edge_weight is None:
                     continue
 
                 previous_cost = costs[vertex]
 
                 if previous_cost is None:
-                    costs[vertex] = heap.insert(edge_weight)
+                    costs[vertex] = heap.insert(Entry(vertex, edge_weight))
                     edges[vertex] = current_vertex
 
-                elif edge_weight < previous_cost:
-                    heap.change_value(costs[vertex], edge_weight)
+                elif edge_weight < previous_cost.value.weight:
+
+                    heap.change_value(
+                        costs[vertex], Entry(vertex, edge_weight))
                     edges[vertex] = current_vertex
 
-        return result.sum
+        return (result, sum)
+
+
+class Entry(object):
+    def __init__(self, vertex, weight):
+        self.vertex = vertex
+        self.weight = weight
+
+    def __iter__(self):
+        return iter((self.vertex, self.weight))
---
+++ test/algorithms/prims_test.py
@@ -0,0 +1,60 @@
+from src.algorithms.prims import Prims
+
+import unittest
+
+
+class TestPrims(unittest.TestCase):
+    def test_really_simple_graph(self):
+        prims = Prims()
+
+        graph = [[None, 2, None, 1], [2, None, None, 2],
+                 [None, None, None, 3], [1, 2, 3, None]]
+
+        result, sum = prims.get_mst(graph)
+
+        self.assertEqual(sum, 6)
+
+    def test_simple_graph(self):
+        prims = Prims()
+
+        graph = [[None, 2, None, 1, 5], [2, None, None, 2, None],
+                 [None, None, None, 3, 2], [1, 2, 3, None, 3], [5, None, 2, 3, None]]
+
+        result, sum = prims.get_mst(graph)
+
+        self.assertEqual(sum, 8)
+
+    def test_complete_graph(self):
+        prims = Prims()
+
+        n = 15
+        graph = [
+            [i + 1 for i in range(j)] for j in range(n)]
+
+        for i in range(n):
+            graph[i].append(None)
+
+            for j in range(len(graph[i]), n):
+                graph[i].append(graph[j][i])
+
+        result, sum = prims.get_mst(graph)
+
+        self.assertEqual(sum, 14)
+
+    def test_weird_complete_graph(self):
+        prims = Prims()
+
+        n = 15
+        graph = [
+            [i + j + 1 for i in range(j)] for j in range(n)]
+
+        for i in range(n):
+            graph[i].append(None)
+
+            for j in range(len(graph[i]), n):
+                graph[i].append(graph[j][i])
+
+        result, result_sum = prims.get_mst(graph)
+
+        expected_sum = sum(range(2, 16))
+        self.assertEqual(expected_sum, result_sum)
