4006cfec295d201d14eb29ba4e1e46af223203de
Gabriel Thibeault <gabriel.eric.thibeault@gmail.com>
2021-12-29 15:49:13 +0100

    Cormen's dijkstra UT

--- test/algorithms/dijkstra_test.py
+++ test/algorithms/dijkstra_test.py
@@ -14,3 +14,16 @@ class TestDijkstra(unittest.TestCase):
 
         self.assertEqual(result.distances, [0, 1, 2])
         self.assertEqual(result.paths, [[], [0], [0, 1]])
+
+    def test_cormens_graph(self):
+        dijkstra = Dijkstra()
+
+        n = 5
+        adjacency = [[(1, 10), (2, 5)], [(2, 2), (3, 1)],
+                     [(1, 3), (3, 9), (4, 2)], [(4, 4)], [(0, 7), (3, 6)]]
+
+        result = dijkstra.find_shortest_path(0, n, adjacency)
+
+        self.assertEqual(result.distances, [0, 8, 5, 9, 7])
+        self.assertEqual(
+            result.paths, [[], [0, 2], [0], [0, 2, 1], [0, 2]])
