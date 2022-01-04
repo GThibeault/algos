1a77d2be9b047b6ad0e77b85387f1744a47ced76
Gabriel Thibeault <gabriel.eric.thibeault@gmail.com>
2022-01-04 18:29:41 +0100

    Disjoint Forest join fix

--- src/data_structures/disjoint_forest.py
+++ src/data_structures/disjoint_forest.py
@@ -6,31 +6,35 @@ from typing import TypeVar, Generic
 T = TypeVar("T")
 
 
+# A single class could easily be used instead of the DisjoinForest/DisjointForestSet model seen below
 class DisjointForest(Generic[T]):
-    def make_set(self, value: T) -> DisjointForestSet[T]:
+    def make_set(self, value: T = None) -> DisjointForestSet[T]:
         return DisjointForestSet(value, 0)
 
     def are_in_same_set(self, set_1: DisjointForestSet[T], set_2: DisjointForestSet[T]) -> bool:
         if set_1 is None or set_2 is None:
             return False
 
-        repr_1, repr_2 = self.get_representative(
-            set_1), self.get_representative(set_2)
+        repr_1, repr_2 = self._get_representative(
+            set_1), self._get_representative(set_2)
 
         return repr_1 is repr_2
 
     def join_sets(self, set_1: DisjointForestSet[T], set_2: DisjointForestSet[T]) -> DisjointForestSet[T]:
-        if set_1.rank < set_2.rank:
-            set_1.parent = set_2
-        elif set_2.rank < set_1.rank:
-            set_2.parent = set_1
-        # set_1.rank == set_2.rank
-        else:
-            set_2.parent, set_1.rank = set_1, set_1.rank + 1
+        repr_1, repr_2 = self._get_representative(
+            set_1), self._get_representative(set_2)
 
-    def get_representative(self, set: DisjointForestSet[T]) -> DisjointForestSet[T]:
+        if repr_1.rank < repr_2.rank:
+            repr_1.parent = repr_2
+        elif repr_2.rank < repr_1.rank:
+            repr_2.parent = repr_1
+        # repr_1.rank == repr_2.rank
+        else:
+            repr_2.parent, repr_1.rank = repr_1, repr_1.rank + 1
+
+    def _get_representative(self, set: DisjointForestSet[T]) -> DisjointForestSet[T]:
         if set.parent is not set:
-            set.parent = self.get_representative(set.parent)
+            set.parent = self._get_representative(set.parent)
 
         return set.parent
 
