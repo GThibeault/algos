e74ff805b09a540f555ab421cfa67a13871ef63b
Gabriel Thibeault <gabriel.eric.thibeault@gmail.com>
2022-01-04 19:03:35 +0100

    Kruskal's fixes

--- src/algorithms/kruskals.py
+++ src/algorithms/kruskals.py
@@ -1,22 +1,21 @@
-from typing import Iterable, List, Tuple
+from __future__ import annotations
+from typing import Iterable, List
 from src.data_structures.disjoint_forest import DisjointForest
 from dataclasses import dataclass, field
-from __future__ import annotations
 
 
 class Kruskals(object):
     def get_mst(self, n: int, edges: Iterable[Edge]) -> KruskalsResult:
         sorted_edges = sorted(edges, key=lambda e: e.weight)
 
-        forest = DisjointForest
+        forest = DisjointForest()
         sets = [forest.make_set(i) for i in range(n)]
 
         res = KruskalsResult()
 
         while len(sorted_edges) > 0:
             current_edge = sorted_edges.pop(0)
-            source_set, target_set = sets[current_edge.source,
-                                          current_edge.target]
+            source_set, target_set = sets[current_edge.source], sets[current_edge.target]
 
             if not forest.are_in_same_set(source_set, target_set):
                 forest.join_sets(source_set, target_set)
