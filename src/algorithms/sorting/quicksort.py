fe9a5c381e7dc0449165cb48295dce079490b7e1
Gabriel Thibeault <gabriel.eric.thibeault@gmail.com>
2022-01-06 01:46:35 +0100

    Add underscore to private methods

--- src/algorithms/sorting/quicksort.py
+++ src/algorithms/sorting/quicksort.py
@@ -9,30 +9,30 @@ class Quicksort:
         if (end - start <= 1):
             return array
 
-        pivot_index = self.get_pivot_index(array, start, end)
+        pivot_index = self._get_pivot_index(array, start, end)
 
-        self.swap(array, start, pivot_index)
+        self._swap(array, start, pivot_index)
 
         pivot_index = start
 
         for i in range(start, end):
             if array[i] <= array[start]:
-                self.swap(array, i, pivot_index)
+                self._swap(array, i, pivot_index)
                 pivot_index += 1
 
         pivot_index -= 1
-        self.swap(array, start, pivot_index)
+        self._swap(array, start, pivot_index)
 
         self.sort(array, start, pivot_index)
         self.sort(array, pivot_index + 1, end)
 
         return array
 
-    def swap(self, array: List[T], l: int, r: int) -> None:
+    def _swap(self, array: List[T], l: int, r: int) -> None:
         if l != r:
             array[l], array[r] = array[r], array[l]
 
-    def get_pivot_index(self, array: List[T], start: int, end: int) -> int:
+    def _get_pivot_index(self, array: List[T], start: int, end: int) -> int:
         size = end - start
 
         options = (start, start + size // 2, end - 1)
